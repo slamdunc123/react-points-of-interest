005478a5a0e358fa4b1c3031828bd07281a4ff17
duncanlaidlaw <slamdunc123@gmail.com>
2022-09-22 21:35:50 +0100

    refactor to create smaller Map component

---
+++ src/Map/Map.js
@@ -0,0 +1,69 @@
+import { Link } from 'react-router-dom';
+
+import {
+	GoogleMap,
+	LoadScript,
+	Marker,
+	InfoWindow,
+} from '@react-google-maps/api';
+
+const MAP_API = process.env.REACT_APP_MAP_API;
+
+const containerStyle = {
+	width: '100vw',
+	height: '100vh',
+};
+
+const center = {
+	lat: 53.19495626366442,
+	lng: -1.695365906570867,
+};
+
+const Map = ({ points, activePoint, handlePointOnClick }) => {
+	return (
+		<LoadScript googleMapsApiKey={MAP_API}>
+			<GoogleMap
+				mapContainerStyle={containerStyle}
+				center={center}
+				zoom={18}
+				mapTypeId='satellite'
+				onClick={() => handlePointOnClick('')} // set to an empty string to avoid object and uncontrolled component warnings
+			>
+				<>
+					{points.map((point) => (
+						<Marker
+							position={{
+								lat: point.lat,
+								lng: point.lng,
+							}}
+							onClick={() => handlePointOnClick(point)}
+							key={point.id}
+						/>
+					))}
+					{activePoint ? (
+						<InfoWindow
+							position={{
+								lat: activePoint.lat,
+								lng: activePoint.lng,
+							}}
+							onCloseClick={() => handlePointOnClick(null)}
+						>
+							<div className='info-container'>
+								<h4>{activePoint.name}</h4>
+								<p>{`Built: ${activePoint.yearBuilt}`}</p>
+
+								<Link to={`/${activePoint.id}`}>
+									read more...
+								</Link>
+							</div>
+						</InfoWindow>
+					) : (
+						<></>
+					)}
+				</>
+			</GoogleMap>
+		</LoadScript>
+	);
+};
+
+export default Map;
--- src/Points/Points.js
+++ src/Points/Points.js
@@ -1,31 +1,13 @@
 import { useState } from 'react';
-import {Link} from 'react-router-dom'
-
-import {
-	GoogleMap,
-	LoadScript,
-	Marker,
-	InfoWindow,
-} from '@react-google-maps/api';
 
 import points from '../points';
 import Sidebar from '../Sidebar/Sidebar';
+import Map from '../Map/Map';
 import styles from './points.module.css';
-
-const MAP_API = process.env.REACT_APP_MAP_API
-
-const containerStyle = {
-	width: '100vw',
-	height: '100vh',
-};
-
-const center = {
-	lat: 53.19495626366442,
-	lng: -1.695365906570867,
-};
 
 const Points = () => {
 	const [activePoint, setActivePoint] = useState(''); // initalise with an empty string to avoit object and uncontrolled component warnings
+
 	const handlePointOnClick = (point) => {
 		setActivePoint(point);
 	};
@@ -41,46 +23,11 @@ const Points = () => {
 				activePoint={activePoint}
 				handlePointOnChange={handlePointOnChange}
 			/>
-			<LoadScript googleMapsApiKey={MAP_API}>
-				<GoogleMap
-					mapContainerStyle={containerStyle}
-					center={center}
-					zoom={18}
-					mapTypeId='satellite'
-					onClick={() => setActivePoint('')} // set to an empty string to avoit object and uncontrolled component warnings
-				>
-					<>
-						{points.map((point) => (
-							<Marker
-								position={{
-									lat: point.lat,
-									lng: point.lng,
-								}}
-								onClick={() => handlePointOnClick(point)}
-								key={point.id}
-							/>
-						))}
-						{activePoint ? (
-							<InfoWindow
-								position={{
-									lat: activePoint.lat,
-									lng: activePoint.lng,
-								}}
-								onCloseClick={() => setActivePoint(null)}
-							>
-								<div className='info-container'>
-									<h4>{activePoint.name}</h4>
-									<p>{`Built: ${activePoint.yearBuilt}`}</p>
-                                    
-                                    <Link to={`/${activePoint.id}`}>read more...</Link>
-								</div>
-							</InfoWindow>
-						) : (
-							<></>
-						)}
-					</>
-				</GoogleMap>
-			</LoadScript>
+			<Map
+				points={points}
+				activePoint={activePoint}
+				handlePointOnClick={handlePointOnClick}
+			/>
 		</div>
 	);
 };
