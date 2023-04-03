59b74c3b7e2bc49b2027c114a1464a6b8cd9c72a
duncanlaidlaw <slamdunc123@gmail.com>
2023-04-03 11:39:29 +0100

    update Point component to use mui

--- src/components/Point/Point.tsx
+++ src/components/Point/Point.tsx
@@ -1,44 +1,78 @@
 import React, { useEffect, useState } from 'react';
-import { useParams, useNavigate, Link } from 'react-router-dom';
+import { useParams, useNavigate, Link as RouterLink } from 'react-router-dom';
 import points from '../../points';
-import styles from './point.module.css'
+import styles from './point.module.css';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
 
 const Point = () => {
-  const { id } = useParams();
-  const navigate = useNavigate();
-  const [point, setPoint] = useState<any>({});
+	const { id } = useParams();
+	const navigate = useNavigate();
+	const [point, setPoint] = useState<any>({});
 
-  const getPoint = () => {
-    const point = points.find((point) => point.id === id);
-    if (!point) {
-      navigate('/');
-    } else setPoint(point);
-  };
+	const getPoint = () => {
+		const point = points.find((point) => point.id === id);
+		if (!point) {
+			navigate('/');
+		} else setPoint(point);
+	};
 
-  useEffect(() => {
-    getPoint();
-  });
+	useEffect(() => {
+		getPoint();
+	});
 
-  return (
-    <div className={styles.container}>
-      <Link to="/">Home</Link>
-      <Link to={`/edit-point/${id}`}>Edit</Link>
-      {point ? (
-        <>
-          <p>{point.name}</p>
-          <p>{`Built: ${point.yearBuilt}`}</p>
-          <p>
-            Website:{' '}
-            <a href={`${point.url}`}>
-              visit
-            </a>
-          </p>
-        </>
-      ) : (
-        'loading...'
-      )}
-    </div>
-  );
+	return (
+		<>
+			<Link component={RouterLink} to='/' underline='none'>
+				Home
+			</Link>
+			<div className={styles.container}>
+				{point ? (
+					<>
+						<Card sx={{ maxWidth: 345 }}>
+							<CardContent>
+								<Typography
+									gutterBottom
+									variant='h5'
+									component='div'
+								>
+									{point.name}
+								</Typography>
+								<Typography
+									variant='body2'
+									color='text.secondary'
+								>
+									{`Built: ${point.yearBuilt}`}
+								</Typography>
+							</CardContent>
+							<CardActions
+								sx={{
+									display: 'flex',
+									justifyContent: 'flex-end',
+								}}
+							>
+								<Link
+									component={RouterLink}
+									to={`/edit-point/${id}`}
+									underline='none'
+								>
+									Edit
+								</Link>
+								<Link href={`${point.url}`} underline='none'>
+									More
+								</Link>
+							</CardActions>
+						</Card>
+					</>
+				) : (
+					'loading...'
+				)}
+			</div>
+		</>
+	);
 };
 
 export default Point;
