d11fa4e4fe805167096556bc1aed2ed35cee3793
duncanlaidlaw <slamdunc123@gmail.com>
2023-04-22 12:06:24 +0100

    remove amplify dependent resourses for api

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,11 +1,4 @@
 export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "reactpointsofinteres": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  },
   "auth": {
     "reactpointsofinteres7eb44a92": {
       "AppClientID": "string",
